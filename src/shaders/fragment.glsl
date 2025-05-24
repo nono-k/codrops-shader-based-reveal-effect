77defd5f9bdb05bbff1671ead2bea625daff096c
iwasaki <poettype73@gmail.com>
2025-05-25 00:59:44 +0900

    Add

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,13 +1,22 @@
-uniform float time;
-uniform float progress;
+uniform float uTime;
+uniform float uProgress;
 uniform sampler2D uTexture;
-uniform vec4 resolution;
 varying vec2 vUv;
-varying vec3 vPosition;
-float PI = 3.141592653589793238;
+
+#include ./includes/perlin3dNoise.glsl
 
 void main() {
+  vec2 displacedUv = vUv + cnoise(vec3(vUv * 5.0, uTime * 0.1));
+  float strength = cnoise(vec3(displacedUv * 5.0, uTime * 0.2));
+
+  float radialGradient = distance(vUv, vec2(0.5)) * 12.5 - 7.0 * uProgress;
+  strength += radialGradient;
+
+  strength = clamp(strength, 0.0, 1.0);
+  strength = 1.0 - strength;
 
   vec3 textureColor = texture2D(uTexture, vUv).rgb;
-  gl_FragColor = vec4(textureColor, 1.0);
+
+  float opacityProgress = smoothstep(0.0, 0.7, uProgress);
+  gl_FragColor = vec4(textureColor, strength * opacityProgress);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,8 +1,16 @@
-uniform float time;
+uniform float uProgress;
 varying vec2 vUv;
 
 void main() {
-  vUv = uv;
+  vec3 newPosition = position;
 
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  float distanceToCenter = distance(vec2(0.5), uv);
+
+  float wave = (1.0 - uProgress) * sin(distanceToCenter * 10.0 - uProgress * 5.0) * 0.1;
+
+  newPosition.z += wave;
+
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+
+  vUv = uv;
 }
